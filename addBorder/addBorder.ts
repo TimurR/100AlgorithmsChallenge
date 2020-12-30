4a0b98d0a176731094f9c05555e68165b341e2fc
RTim <timur.rasulev.82@gmail.com>
2020-12-30 17:08:40 +0300

    add borders

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,9 @@
 function addBorder(picture: string[]): string[] {
+    const borderLong = Array(picture[0].length + 2).fill('*').join('');
+    const borderedValues = picture.map(val=> `*${val}*`);
+
+    return [borderLong, ...borderedValues, borderLong];
 
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
