3adb16360a4d7f42edf873b1a7f83b9ad2fcaa0c
RTim <timur.rasulev.82@gmail.com>
2021-01-01 18:27:31 +0300

    almost increasing sequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,14 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let amountToDelete = 0;
+    sequence.forEach((curr, i) => {
+        if(curr > sequence[++i] ) {
+            amountToDelete = amountToDelete + 1;
+        }
+    });
+
+    return amountToDelete <= 1;
 
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
