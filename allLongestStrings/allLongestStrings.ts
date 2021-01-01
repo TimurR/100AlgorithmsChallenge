69159d7e6b1413ca614a1b3a2d01a3e8f9c5de0e
RTim <timur.rasulev.82@gmail.com>
2021-01-01 17:51:25 +0300

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,17 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    const longestStrings = inputArray.sort((a, b) => {
+        if (a.length < b.length) {
+            return 1
+        }
+        if (b.length < a.length) {
+            return -1
+        }
+
+        return 0;
+    })[0].length
+
+    return inputArray.filter((val) => val.length === longestStrings);
 
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "fghj", "vcd", "aba"]));
