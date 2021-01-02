d98ee185ddad641829491a0046451b53ab88f54c
RTim <timur.rasulev.82@gmail.com>
2021-01-02 20:06:41 +0300

    alphabetic shift

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,7 +1,7 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
     let amountToDelete = 0;
     sequence.forEach((curr, i) => {
-        if(curr > sequence[++i] ) {
+        if(curr >= sequence[++i] ) {
             amountToDelete = amountToDelete + 1;
         }
     });
@@ -10,5 +10,5 @@ function almostIncreasingSequence(sequence: number[]): boolean {
 
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 3, 7, 10])) 
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,13 @@
 function alphabeticShift(inputString: string): string {
+    const input = inputString.split('');
 
+    return input.map(l => {
+        if(l.charCodeAt(0) < 122) {
+            return String.fromCharCode(l.charCodeAt(0) + 1)
+        } else {
+            return 'a';
+        }
+    }).join('');
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('crazyz'));
