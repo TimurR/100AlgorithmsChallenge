dddf13d9e34b37b7e14f7f4eac5264a5d3b9e97d
RTim <timur.rasulev.82@gmail.com>
2021-01-03 17:16:30 +0300

    Are Equally Strong?

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,10 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    const me = yourLeft + yourRight;
+    const friend = friendsLeft + friendsRight;
 
+    return me === friend;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
