2bebd1f19249e447abee0aef9288f110534d8fdc
RTim <timur.rasulev.82@gmail.com>
2021-01-03 17:04:47 +0300

    Alternating Sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,4 +1,8 @@
 function alternatingSums(a: number[]): number[] {
+    const first = a.filter((_, i) => (i+1) % 2 !== 0).reduce((acc, current) => acc + current, 0);
+    const second = a.filter((_, i) => (i+1) % 2 === 0).reduce((acc, current) => acc + current, 0);
+    
+    return [first, second];
 
 }
 
