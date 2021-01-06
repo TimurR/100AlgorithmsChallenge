3fada3b72be3b5de2d00bdddfa1c72f5475382e0
RTim <timur.rasulev.82@gmail.com>
2021-01-06 19:37:35 +0300

    Array Max Consecutive Sum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,4 +1,17 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let sumResults = [];
+
+    for(let i = 0; i <= inputArray.length; i++) {
+        if(inputArray[i+1] !== undefined) {
+            sumResults.push(sumOfElements(inputArray.slice(i, i+k)))
+        }
+    }
+
+    return Math.max.apply(null,sumResults);
+}
+
+function sumOfElements(seq: number[]): number {
+    return seq.reduce((acc, curr) => acc + curr, 0);
 
 }
 
