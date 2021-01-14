eb6183c7a7d157124a1f19f17f49c84c09b4716a
RTim <timur.rasulev.82@gmail.com>
2021-01-14 22:34:32 +0300

    Array previous less

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,13 @@
 function arrayPreviousLess(items: number[]): number[] {
+    const result: number[] = [];
+
+    for(let i=0; i < items.length; i++) {
+        result.push(items[i-1] < items[i] ? items[i-1] : -1 );
+    }
+
+    return result;
 
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([6, 5, 2, 10, 5]));
